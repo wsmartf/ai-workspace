7bed023d08060b38ba4f701e17ad8ce969715a35
Will Smart <wjsmart12@gmail.com>
2025-04-13 15:41:00 -0500

    Add isDev mode to not make GPT requests

--- ui/src/utils/server.ts
+++ ui/src/utils/server.ts
@@ -1,4 +1,17 @@
+const isDev = true;
+
 export async function fetchFromServer(endpoint: string, body: any): Promise<any> {
+
+    // TODO: Remove this
+    if (isDev) {
+        // Mock response for development
+        return {
+            "reply": "This is a mock response from the server.",
+            "edit_summary": "This is a mock edit summary.",
+            "updated_doc": "This is a mock updated document.",
+        };
+    }
+
     try {
         const res = await fetch(`http://localhost:11434${endpoint}`, {
             method: "POST",
